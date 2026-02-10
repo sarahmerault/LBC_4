6b8069824cd0b18ae2b018317007c8afc033de7e
sarahmerault <sarah.merault@gmail.com>
2026-02-10 14:25:05 +0100

    Delete src directory

--- src/server.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js';
-import "dotenv/config"
-
-const PORT = process.env.PORT || 5000;
-
-//ATTRIBUER UN PORT 
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT} `);
-});
